c7405fada9c804f3ffb7b710e3c166c2b31706d6
Olivier Matz <omatz@fastmail.com>
2023-09-16 15:59:04 -0400

    Rename to HttpTrigger.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.function_name(name="HttpExample")
+@app.function_name(name="HttpTrigger")
 @app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("HttpExample function processed a request!")
